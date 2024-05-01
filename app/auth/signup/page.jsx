37af6579852adba0999deb2485f96aae7a94b004
Solomon13693 <adeoyesolomon2693@gmail.com>
2024-05-01 12:43:26 +0100

    commit

--- app/auth/signup/page.jsx
+++ app/auth/signup/page.jsx
@@ -1,5 +1,6 @@
 'use client'
-import React, { useState } from 'react';
+
+import React, { useState, Suspense } from 'react';
 import AuthHeader from '@/components/auth/AuthHeader';
 import Button from '@/components/ui/buttons/Button';
 import accounts from '@/json/account'
@@ -32,70 +33,49 @@ const SignUpPage = () => {
     };
 
     return (
-        <>
+        <Suspense fallback={<div>Loading...</div>}>
+            <>
+                <AuthHeader label='Sign in' link='signin' />
 
-            <AuthHeader label='Sign in' link='signin' />
-
-            {!account && (
-                <div className="relative flex items-center p-0 overflow-hidden bg-center bg-cover">
-
-                    <div className="container mx-auto z-1">
-
-                        <div className="relative flex flex-wrap">
-
-                            <div className="relative flex flex-col w-full max-w-full px-6 mx-auto lg:mx-12 py-10">
-
-                                <div className="relative pt-6 md:flex-0 w-full sm:w-[75%] xl:w-[60%] m-auto">
-
-                                    <div className="bg-secondary rounded-xl p-5 mx-auto">
-
-                                        <h2 className="text-base md:text-lg text-dark font-bold tracking-tighter text-center">
-                                            How would you like to use Amorad ? ✨
-                                        </h2>
-
-                                        <div className="flex flex-wrap lg:flex-nowrap items-center gap-4 mt-6">
-
-                                            <EachElement of={accounts} render={(item, index) => (
-
-                                                <AccountType
-                                                    item={item}
-                                                    isSelected={role === item.value}
-                                                    onSelect={handleAccountTypeSelect}
-                                                />
-
-                                            )} />
-
+                {!account && (
+                    <div className="relative flex items-center p-0 overflow-hidden bg-center bg-cover">
+                        <div className="container mx-auto z-1">
+                            <div className="relative flex flex-wrap">
+                                <div className="relative flex flex-col w-full max-w-full px-6 mx-auto lg:mx-12 py-10">
+                                    <div className="relative pt-6 md:flex-0 w-full sm:w-[75%] xl:w-[60%] m-auto">
+                                        <div className="bg-secondary rounded-xl p-5 mx-auto">
+                                            <h2 className="text-base md:text-lg text-dark font-bold tracking-tighter text-center">
+                                                How would you like to use Amorad ? ✨
+                                            </h2>
+                                            <div className="flex flex-wrap lg:flex-nowrap items-center gap-4 mt-6">
+                                                <EachElement of={accounts} render={(item, index) => (
+                                                    <AccountType
+                                                        item={item}
+                                                        isSelected={role === item.value}
+                                                        onSelect={handleAccountTypeSelect}
+                                                    />
+                                                )} />
+                                            </div>
+                                            {error && <div className="text-red-600 text-xs font-light text-center mt-2">{error}</div>}
+                                            <Button
+                                                onClick={handleContinueClick}
+                                                color="btn-primary"
+                                                className="py-2.5 min-w-72 m-auto mt-6"
+                                            >
+                                                Continue
+                                            </Button>
                                         </div>
-
-                                        {error && <div className="text-red-600 text-xs font-light text-center mt-2">{error}</div>}
-
-                                        <Button
-                                            onClick={handleContinueClick}
-                                            color="btn-primary"
-                                            className="py-2.5 min-w-72 m-auto mt-6"
-                                        >
-                                            Continue
-                                        </Button>
-
                                     </div>
-
                                 </div>
-
-
                             </div>
-
                         </div>
-
-
                     </div>
+                )}
 
-                </div>
-            )}
-
-            {account === 'practitioner' && <PractitionerAccount />}
-            {account === 'organization' && <OrganizationAccount />}
-
-        </>
+                {account === 'practitioner' && <PractitionerAccount />}
+                {account === 'organization' && <OrganizationAccount />}
+            </>
+        </Suspense>
     );
 };
 
