30f48b93547927d95b4c77906976d74563aa0ec3
Solomon13693 <adeoyesolomon2693@gmail.com>
2024-05-31 19:02:52 +0100

    commit

--- components/dashboard/organization/patient/PatientAction.jsx
+++ components/dashboard/organization/patient/PatientAction.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
+import { Menu, MenuItem } from '@mui/material';
 import ForwardIcon from "@/components/icons/ForwardIcon";
 import EditIcon from "@/components/icons/EditIcon";
 import { PlusCircleIcon } from '@heroicons/react/24/outline';
@@ -6,62 +7,59 @@ import Link from 'next/link';
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
 
 const PatientAction = ({ data }) => {
-    const [open, setOpen] = useState(false);
-    const [position, setPosition] = useState('down');
-    const dropdownRef = useRef(null);
 
-    const toggleDropdown = () => {
-        setOpen(!open);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
     };
 
-    useEffect(() => {
-        if (dropdownRef.current) {
-            const dropdownRect = dropdownRef.current.getBoundingClientRect();
-            const viewportHeight = window.innerHeight;
-            const spaceBelow = viewportHeight - dropdownRect.bottom;
-            const spaceAbove = dropdownRect.top;
-            if (spaceBelow < dropdownRect.height && spaceAbove > dropdownRect.height) {
-                setPosition('up');
-            } else {
-                setPosition('down');
-            }
-        }
-    }, [open]);
-
     return (
-        <div className="relative inline-block text-left">
-            <div onClick={toggleDropdown} className="flex items-center gap-x-2 text-textColor cursor-pointer">
-                <span className="block text-xs font-medium">Expand</span>
+        <>
+            <div onClick={handleClick} className="flex cursor-pointer items-center gap-x-2 text-textColor">
+                <span className="block text-xs font-medium">
+                    Expand
+                </span>
                 <ChevronDownIcon className="w-3 h-3" />
             </div>
-            {open && (
-                <div
-                    ref={dropdownRef}
-                    className={`absolute ${position === 'up' ? 'bottom-full mb-2' : 'top-full mt-2'} w-48 bg-white rounded-lg border border-gray-200 z-50`}
-                >
-                    <div className="py-1">
-                        <Link href={`patient/${data.mrn}`}>
-                            <div className="flex items-center gap-x-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer">
-                                <ForwardIcon className='w-4 h-4' />
-                                <span>View Details</span>
-                            </div>
-                        </Link>
-                        <Link href={`patient/${data.mrn}/edit`}>
-                            <div className="flex items-center gap-x-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer">
-                                <EditIcon className='w-4 h-4' />
-                                <span>Edit Patient</span>
-                            </div>
-                        </Link>
-                        <Link href={`patient/${data.mrn}/study`}>
-                            <div className="flex items-center gap-x-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer">
-                                <PlusCircleIcon className='w-4 h-4' />
-                                <span>Add Patient Study</span>
-                            </div>
-                        </Link>
-                    </div>
+            <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+                PaperProps={{
+                    style: {
+                        boxShadow: 'none', // Remove shadow
+                        borderRadius: '0.5rem', // Customize border radius
+                        border: '1px solid #F2F2F2'
+                    },
+                }}
+            >
+                <div className="py-1">
+                    <Link className='w-full whitespace-nowrap' href={`patient/${data.mrn}`}>
+                        <div className="flex items-center gap-x-2 px-4 py-2 text-xs text-gray-700 hover:bg-gray-100 cursor-pointer">
+                            <ForwardIcon className='w-4 h-4' />
+                            <span>View Details</span>
+                        </div>
+                    </Link>
+                    <Link className='w-full whitespace-nowrap' href={`patient/${data.mrn}/edit`}>
+                        <div className="flex items-center gap-x-2 px-4 py-2 text-xs text-gray-700 hover:bg-gray-100 cursor-pointer">
+                            <EditIcon className='w-4 h-4' />
+                            <span>Edit Patient</span>
+                        </div>
+                    </Link>
+                    <Link className='w-full whitespace-nowrap' href={`patient/${data.mrn}/study`}>
+                        <div className="flex items-center gap-x-2 px-4 py-2 text-xs text-gray-700 hover:bg-gray-100 cursor-pointer">
+                            <PlusCircleIcon className='w-4 h-4' />
+                            <span>Add Patient Study</span>
+                        </div>
+                    </Link>
                 </div>
-            )}
-        </div>
+            </Menu>
+        </>
     );
 };
 
